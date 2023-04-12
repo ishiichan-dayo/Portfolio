9ea4324bebba4821f20d0649c9104c01cb2f1c3a
Yuki Ishii <ishidondon@gmail.com>
2023-04-12 19:18:21 +0900

    Add _app.tsx _document.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Test from "@/components/testComponent";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>Welcome to Next.js!</div>
+      <Test />
+    </>
+  );
+};
+
+export default HomePage;
